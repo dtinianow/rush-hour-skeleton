cd9a4198c7f819df57a364523111eb02411b105a
David Tinianow <david.tinianow@gmail.com>
2016-07-07 16:00:00 -0600

    started iteration 4

---
+++ app/controllers/rush_hour_app.rb
@@ -0,0 +1,18 @@
+class RushHourApp < Sinatra::Base
+
+  post '/sources' do
+    client = Client.new(params)
+    if client.save
+      #also need to check that identifier does not already exist
+      status 200
+      body "{'identifier':'#{params[:identifier]}'}"
+    elsif
+      #identifier already exists
+      status 403
+    elsif
+      #missing parameters
+      status 400
+    end
+  end
+
+end
---
+++ test/controllers/rush_hour_app_test.rb
@@ -0,0 +1,32 @@
+require './test/test_helper'
+require 'pry'
+
+class RushHourAppTest
+  include Rack::Test::Methods
+
+  def app
+    RushHourApp
+  end
+
+  def test_create_a_new_client_with_valid_attributes
+    post '/sources', { identifier: 'jumpstart' }
+    assert_equal 200, last_response.status
+    assert_equal "{'identifier':'jumpstart'}", last_response.body
+  end
+
+  def test_cannot_create_a_client_with_missing_attributes
+    skip
+    post '/sources', { identifier: 'jumpstart'}
+    assert_equal 400, last_response.status
+    post '/sources', { root_url: ''}
+  end
+
+  def test_cannot_create_new_client_with_already_existing_identifier
+    skip
+    post '/sources', { identifier: 'jumpstart' }
+    assert_equal 403, last_response.status
+  end
+
+
+
+end
