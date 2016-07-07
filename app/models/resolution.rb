ae2896835ef5927159072072687778b80f5bc987
tgisg <soniagupta504@gmail.com>
2016-07-07 03:30:53 -0600

    finished uagent class methods and tests

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Resolution < ActiveRecord::Base
   has_many :payload_requests
 
--- app/models/u_agent.rb
+++ app/models/u_agent.rb
@@ -1,6 +1,15 @@
-class UAgent < ActiveRecord::Base  
+class UAgent < ActiveRecord::Base
   has_many :payload_requests
 
   validates :browser, presence: true
   validates :operating_system, presence: true
+
+  def self.browser_breakdown
+    all.select(:browser).joins(:payload_requests).where("u_agents.id" == "payload_request.u_agent_id").group(:browser).count
+  end
+
+  def self.os_breakdown
+    all.select(:operating_system).joins(:payload_requests).where("u_agents.id" == "payload_request.u_agent_id").group(:operating_system).count
+  end
+
 end
