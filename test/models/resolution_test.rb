e89484193070198d78748c720873da86b8425c82
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-07 14:07:39 -0600

    Add helper method of raw_data to test_helper and remove from other tests

--- test/models/resolution_test.rb
+++ test/models/resolution_test.rb
@@ -3,20 +3,6 @@ require_relative '../test_helper.rb'
 class ResolutionTest < Minitest::Test
   include TestHelpers, DataProcessor
 
-  def raw_data
-    '{
-      "url": "http://jumpstartlab.com/blog",
-      "requestedAt": "2013-02-16 21:38:28 -0700",
-      "respondedIn": 37,
-      "referredBy": "http://jumpstartlab.com",
-      "requestType": "GET",
-      "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-      "resolutionWidth": "1920",
-      "resolutionHeight": "1280",
-      "ip": "63.29.38.211"
-     }'
-  end
-
   def test_it_can_hold_width_and_height
     resolution = Resolution.create(width: 1280, height: 800)
     assert_equal 1280, resolution.width
