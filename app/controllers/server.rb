9ecfd7ad8b558c0767a822299c028cf4a6018e8a
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-08 08:41:19 -0600

    Fixed issue with server

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -1,7 +1,36 @@
+require './app/models/data_processor'
+
 module RushHour
   class Server < Sinatra::Base
+    include DataProcessor
+
     not_found do
       erb :error
+    end
+
+    post '/sources' do
+      data = clean_client_data(params)
+      client = Client.new(data)
+      #rootUrl vs root_url
+      if Client.find_by(identifier: params[:identifier])
+        #identifier already exists
+        status 403
+      elsif client.save
+        #also need to check that identifier does not already exist
+        status 200
+        body "{'identifier':'#{params[:identifier]}'}"
+      else
+        #missing parameters
+        status 400
+      end
+    end
+
+    error 403 do
+      "Identifier Already Exists"
+    end
+
+    error 400 do
+      "Missing Parameters"
     end
 
   end
