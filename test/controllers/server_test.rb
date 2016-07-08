c95d758e61c797d1228ecdbf8edbce3b513ff46a
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-08 08:41:39 -0600

    Renamed rush_hour_app_test

---
+++ test/controllers/server_test.rb
@@ -0,0 +1,37 @@
+require './test/test_helper'
+require 'pry'
+
+class RushHourAppTest < Minitest::Test
+  include Rack::Test::Methods, TestHelpers
+
+  def app
+    RushHour::Server
+  end
+
+  def test_create_a_new_client_with_valid_attributes
+    post '/sources', { identifier: 'jumpstart', rootUrl:'http://jumpstartlab.com' }
+    assert_equal 200, last_response.status
+    assert_equal "{'identifier':'jumpstart'}", last_response.body
+  end
+
+  def test_cannot_create_a_client_with_missing_attributes
+    post '/sources', { identifier: 'jumpstart'}
+    assert_equal 400, last_response.status
+    post '/sources', { rootUrl: 'http://jumpstartlab.com'}
+    assert_equal 400, last_response.status
+    assert_equal "Missing Parameters", last_response.body
+  end
+
+  def test_cannot_create_new_client_with_already_existing_identifier
+    Client.create( {identifier: 'jumpstart', root_url: 'http://jumpstartlab.com'} )
+    post '/sources', { identifier: 'jumpstart', rootUrl: 'http://jumpstartlab.com' }
+    assert_equal 403, last_response.status
+    assert_equal "Identifier Already Exists", last_response.body
+  end
+
+  def test_it_can_respond_to_a_curl_request
+    skip
+  end
+
+
+end
