ade61fee55c34c04508be9121506417cd0163cfe
tgisg <soniagupta504@gmail.com>
2016-07-09 14:42:57 -0600

    playing with through

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,11 @@
 class Client < ActiveRecord::Base
   has_many :payload_requests
+  has_many :ips, through: :payload_requests
+  has_many :referred_bies, through: :payload_requests
+  has_many :request_types, through: :payload_requests
+  has_many :resolutions, through: :payload_requests
+  has_many :u_agents, through: :payload_requests
+  has_many :urls, through: :payload_requests
 
   validates :identifier, presence: true
   validates :root_url, presence: true
