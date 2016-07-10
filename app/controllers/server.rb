1f5b0afb0ec6dbe9b85e531ac48c4c000554ae0f
David Tinianow <david.tinianow@gmail.com>
2016-07-10 15:35:22 -0600

    added tests for server

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -67,35 +67,5 @@ module RushHour
         erb :'identifier/relative_path'
       end
     end
-
-    # def status_code_and_message(code, message)
-    #   status code
-    #   body message
-    # end
-    #
-    # def process_client(client, id)
-    #   if Client.find_by(identifier: id)
-    #     status_code_and_message(403, "Identifier '#{id}' already exists")
-    #   elsif client.save
-    #     status_code_and_message(200, "{'identifier':'#{id}'}")
-    #   else
-    #     status_code_and_message(400, "#{client.errors.full_messages.join(', ')}")
-    #   end
-    # end
-    #
-    # def process_client_payload(client, identifier, payload_data)
-    #   if client.nil?
-    #     status_code_and_message(403, "Client with identifier '#{identifier}' is not yet registered.")
-    #   elsif payload_data.nil?
-    #     status_code_and_message(400, "Payload missing - a payload is required")
-    #   elsif PayloadRequest.find_by(foreign_table_ids(payload_data))
-    #     status_code_and_message(403, "Record with the supplied payload already exists")
-    #   else
-    #     payload = process_payload(params[:payload])
-    #     PayloadRequest.find(payload.id).update_attribute(:client_id, client.id)
-    #     status_code_and_message(200, "Payload entered into database")
-    #   end
-    # end
-
   end
 end
--- test/controllers/server_test.rb
+++ test/controllers/server_test.rb
@@ -51,9 +51,48 @@ class RushHourAppTest < Minitest::Test
   def test_responds_with_200_status_when_unique_payload_and_existing_client
     Client.create( {identifier: 'jumpstart', root_url: 'http://jumpstartlab.com'} )
     post '/sources/jumpstart/data', {payload: raw_data}
-
     assert_equal 200, last_response.status
     assert_equal 'Payload entered into database', last_response.body
+  end
+
+  def test_responds_with_index_if_client_exists_and_payload_is_registered
+    Client.create( {identifier: 'jumpstartlab', root_url: 'http://jumpstartlab.com'} )
+    data = process_foreign_tables(raw_data)
+    data[:client_id] = 1
+    PayloadRequest.create(data)
+    get '/sources/jumpstartlab'
+    assert_equal 200, last_response.status
+    assert last_response.body.include?("Data for 'jumpstartlab'")
+  end
+
+  def test_responds_with_error_message_if_client_does_not_exist
+    get '/sources/jumpstartlab'
+    assert_equal 200, last_response.status
+    assert last_response.body.include?('jumpstartlab does not exist')
+  end
+
+  def test_responds_with_error_message_if_no_payload_is_registered_for_client
+    Client.create( {identifier: 'jumpstartlab', root_url: 'http://jumpstartlab.com'} )
+    get '/sources/jumpstartlab'
+    assert_equal 200, last_response.status
+    assert last_response.body.include?('No payload registered for jumpstart')
+  end
+
+  def test_responds_with_error_message_if_no_path_exists_for_identifier
+    Client.create( {identifier: 'jumpstartlab', root_url: 'http://jumpstartlab.com'} )
+    get '/sources/jumpstartlab/urls/nothing'
+    assert_equal 200, last_response.status
+    assert last_response.body.include?("The path 'nothing' for 'jumpstartlab' has not been requested.")
+  end
+
+  def test_responds_with_relative_path_page_if_path_exists_for_identifier
+    Client.create( {identifier: 'jumpstartlab', root_url: 'http://jumpstartlab.com'} )
+    data = process_foreign_tables(raw_data)
+    data[:client_id] = 1
+    PayloadRequest.create(data)
+    get '/sources/jumpstartlab/urls/blog'
+    assert_equal 200, last_response.status
+    assert last_response.body.include?('Max. response time for this URL:')
   end
 
 end
